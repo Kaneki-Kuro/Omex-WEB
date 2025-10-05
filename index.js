958e2baa2435ad6141e5c86751daf9154bf32b92
Kaneki-Kuro <mukesh.79.nbh.coomay@gmail.com>
2025-10-05 18:28:49 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import serverRoutes from "./routes/server.js";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Routes
+app.use("/api/server", serverRoutes);
+
+app.listen(process.env.PORT, () => console.log(`Backend running on port ${process.env.PORT}`));
